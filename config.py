d6debd6276d4a3c4f344f82b80a03c5942243e0a
d.chaenkova <dash.kustowa2010@yandex.ru>
2024-07-15 22:25:06 +0300

    Update config.py
    add some secure :)

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 context = os.getenv('context', 'bstack')
 platform = os.getenv('platform','android')
-bstack_userName = os.getenv('bstack_userName', 'qnamail_1Rq3z8')
-bstack_accessKey = os.getenv('bstack_accessKey', 'Js2iRafW5MtQdVArsBrX')
+bstack_userName = os.getenv('bstack_userName')
+bstack_accessKey = os.getenv('bstack_accessKey')
 base_url = os.getenv('base_url', 'https://www.wikipedia.org')
